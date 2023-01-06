1f4fe1afc88ca3e7c9447918c714450ef8926d33
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-01-06 17:00:21 -0400

    Code challenge submission for question: 2837 Total Traveled Distance

---
+++ 2837 Total Traveled Distance/2837total-traveled-distance.sql
@@ -0,0 +1,8 @@
+SELECT u.user_id, 
+       u.name,
+       IFNULL(SUM(distance), 0) AS 'traveled distance'
+FROM Users AS u
+LEFT JOIN Rides AS r
+ON u.user_id = r.user_id
+GROUP BY user_id, name
+ORDER BY user_id 
