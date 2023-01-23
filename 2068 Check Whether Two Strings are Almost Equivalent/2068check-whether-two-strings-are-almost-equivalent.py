ceb41094f2567fd93cf2fc8263b760ac557f1840
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-01-23 17:00:06 -0400

    Code challenge submission for question: 2068 Check Whether Two Strings are Almost Equivalent

---
+++ 2068 Check Whether Two Strings are Almost Equivalent/2068check-whether-two-strings-are-almost-equivalent.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        for i in word1:
+            if abs(word1.count(i)-word2.count(i))>3:
+                return False
+        for i in word2:
+            if abs(word2.count(i)-word1.count(i))>3:
+                return False
+        return True
