09f11a008c4f7d8fd4ef214c7dfac321668a0c04
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-02-12 15:20:49 -0400

    Code challenge submission for question: 1550 Three Consecutive Odds

---
+++ 1550 Three Consecutive Odds/1550three-consecutive-odds.py
@@ -0,0 +1,7 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr):
+        n = len(arr)
+        for i in range(n-2):
+            if arr[i]%2==1 and arr[i+1]%2==1 and arr[i+2]%2==1:
+                return True
+        return False
