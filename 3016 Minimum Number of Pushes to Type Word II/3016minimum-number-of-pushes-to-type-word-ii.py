555bdaa873865bc501d2b8d2a0f9d43052099e5c
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-02-23 22:57:45 -0400

    Code challenge submission for question: 3016 Minimum Number of Pushes to Type Word II

---
+++ 3016 Minimum Number of Pushes to Type Word II/3016minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumPushes(self, word):
+        frequency = [0]*26
+        for c in word:
+            frequency[ord(c)-ord("a")]+=1
+        frequency.sort(reverse = True)
+        total_pushes = 0
+        for i in range(26):
+            if frequency[i]==0:
+                break
+            total_pushes+=(i//8+1)*frequency[i]
+        return total_pushes
+
+        
