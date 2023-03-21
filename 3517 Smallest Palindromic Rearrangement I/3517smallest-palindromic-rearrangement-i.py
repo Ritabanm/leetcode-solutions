3d9427faa85b2bd875f47bf511d4c388b307d8f4
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-03-20 21:01:50 -0400

    Code challenge submission for question: 3517 Smallest Palindromic Rearrangement I

---
+++ 3517 Smallest Palindromic Rearrangement I/3517smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def smallestPalindrome(self, s):
+        partition = len(s)//2
+        base = sorted(s[:partition])
+        mid = [s[partition]] if len(s)%2==1 else []
+        reversed_base = base[::-1]
+        return "".join(base + mid + reversed_base)
