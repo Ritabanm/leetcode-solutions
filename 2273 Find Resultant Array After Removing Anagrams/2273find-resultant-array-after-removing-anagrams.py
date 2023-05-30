d22f95ccb6dcbc4fd5aa39cd60cbb36de786551b
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-05-30 16:56:12 -0400

    Code challenge submission for question: 2273 Find Resultant Array After Removing Anagrams

---
+++ 2273 Find Resultant Array After Removing Anagrams/2273find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        x = [words[0]]
+        for i in range(1, len(words)):
+            if sorted(x[-1])!=sorted(words[i]):
+                x.append(words[i])
+        return x
