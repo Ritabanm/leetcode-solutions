36f4344c133c395971e51897e86db52a3e0afcec
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-06-14 11:43:55 -0400

    Code challenge submission for question: 133 Clone Graph

---
+++ 133 Clone Graph/133clone-graph.py
@@ -0,0 +1,12 @@
+class Solution:
+    def cloneGraph(self, node):
+        oldtonew = {}
+        def dfs(node):
+            if node in oldtonew:
+                return oldtonew[node]
+            copy = Node(node.val)
+            oldtonew[node]=copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        return dfs(node) if node else None
