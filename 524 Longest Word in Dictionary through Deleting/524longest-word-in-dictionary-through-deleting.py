49756d005f3b8b707319c44a444b42ebf9ffaab6
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-06-28 18:41:40 -0400

    Code challenge submission for question: 524 Longest Word in Dictionary through Deleting

---
+++ 524 Longest Word in Dictionary through Deleting/524longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def findLongestWord(self, S, D):
+        D.sort(key = lambda x: (-len(x), x))
+        for word in D:
+            i = 0
+            for c in S:
+                if i < len(word) and word[i] == c:
+                    i += 1
+            if i == len(word):
+                return word
+        return ""
