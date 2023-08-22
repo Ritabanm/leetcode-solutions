a61157b321b3f0c843adb8472a4090119abefd7f
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-08-22 17:09:41 -0400

    Code challenge submission for question: 2207 Maximize Number of Subsequences in a String

---
+++ 2207 Maximize Number of Subsequences in a String/2207maximize-number-of-subsequences-in-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumSubsequenceCount(self, text: str, pattern: str) -> int:
+        count0 = count1 = countPattern = 0
+        for c in text:
+            if c == pattern[1]:
+                count1 += 1
+                countPattern += count0
+            if c == pattern[0]:
+                count0 += 1
+        
+        return countPattern + max(count1, count0)
