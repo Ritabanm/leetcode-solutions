b599544ce0a8e83a30c5ca9a85663aee6c087666
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-09-02 22:43:42 -0400

    Code challenge submission for question: 2162 Minimum Cost to Set Cooking Time

---
+++ 2162 Minimum Cost to Set Cooking Time/2162minimum-cost-to-set-cooking-time.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCostSetTime(self, startAt: int, moveCost: int,
+                             pushCost: int, targetSeconds: int) -> int:
+
+        def cost(m: int,s: int)-> int:
+
+            if not(0 <= m <= 99 and 0 <= s <= 99): return inf
+
+            display = str(startAt) + str(100*m+s)
+
+            moves  = sum(display[i] != display[i-1] for i in range(1,len(display)))
+            pushes = len(display) - 1
+            
+            return moveCost*moves + pushCost*pushes
+        
+
+        mins, secs = divmod(targetSeconds,60)
+            
+        return  min(cost(mins,secs),cost(mins-1,secs+60))
