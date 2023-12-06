d79f25b71dcf91524412b376242087fc05e05409
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-12-05 20:46:23 -0400

    Code challenge submission for question: 2274 Maximum Consecutive Floors Without Special Floors

---
+++ 2274 Maximum Consecutive Floors Without Special Floors/2274maximum-consecutive-floors-without-special-floors.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxConsecutive(self, bottom: int, top: int, special: List[int]) -> int:
+        special.sort()
+        res = special[0]-bottom
+        for i in range(1, len(special)):
+            res = max(res, special[i]-special[i-1]-1)
+        return max(res, top-special[-1])
