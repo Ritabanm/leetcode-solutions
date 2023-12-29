326d34ec07df787a373a7502665f9bcccdb2a484
Ritaban Mitra <ritabanmitra99@gmail.com>
2023-12-29 17:02:34 -0400

    Code challenge submission for question: 141 Linked List Cycle

---
+++ 141 Linked List Cycle/141linked-list-cycle.py
@@ -0,0 +1,27 @@
+"""
+class Solution:
+    def hasCycle(self, head):
+        if not head:
+            return None
+        
+        slow = head
+        fast = head.next
+        while slow!=fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True"""
+
+class Solution:
+    def hasCycle(self, head):
+        if not head:
+            return None
+        slow = head
+        fast = head.next
+        while slow!=fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
