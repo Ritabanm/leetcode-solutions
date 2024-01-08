a4bb553d642e854d1e47cb74be6019c7791a7a88
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-01-07 22:37:34 -0400

    Code challenge submission for question: 1672 Richest Customer Wealth

---
+++ 1672 Richest Customer Wealth/1672richest-customer-wealth.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        for account in accounts:
+            curr_cust_wealth= sum(account)
+            max_wealth = max(max_wealth, curr_cust_wealth)
+        return max_wealth
