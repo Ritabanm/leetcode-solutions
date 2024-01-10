d18f6ffdbcf40c2d6ffb4c6389645281ee2f3a36
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-01-10 11:03:03 -0400

    Code challenge submission for question: 374 Guess Number Higher or Lower

---
+++ 374 Guess Number Higher or Lower/374guess-number-higher-or-lower.py
@@ -0,0 +1,15 @@
+class Solution:
+    def guessNumber(self, n:int)->int:
+        l,r = 1, n
+
+        while l<=r:
+            m = (l+r)//2
+            res = guess(m)
+            if res<0:
+                r = m-1
+            elif res>0:
+                l = m+1
+            else:
+                return m
+        return -1
+
