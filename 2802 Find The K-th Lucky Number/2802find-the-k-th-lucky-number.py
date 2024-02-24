11f785b5767885b353052b98b5fc2d46c20d86e0
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-02-24 14:24:50 -0400

    Code challenge submission for question: 2802 Find The K-th Lucky Number

---
+++ 2802 Find The K-th Lucky Number/2802find-the-k-th-lucky-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthLuckyNumber(self, k: int) -> str:
+        # Increment k to account for 1-based indexing
+        k = k + 1
+
+        # For each digit in the binary representation of k except the most significant
+        # Prepend 4 to the result if the digit is 0 and 7 otherwise
+        kth_lucky_num = ""
+        while k > 1:
+            kth_lucky_num = "".join((("7" if k & 1 else "4"), kth_lucky_num))
+            k >>= 1
+        return kth_lucky_num
