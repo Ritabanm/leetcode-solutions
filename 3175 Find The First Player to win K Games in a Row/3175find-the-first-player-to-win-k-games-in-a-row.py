36f57e0d5ce4a99e63a833ec8cdfba6b3b772299
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-04-09 19:01:02 -0400

    Code challenge submission for question: 3175 Find The First Player to win K Games in a Row

---
+++ 3175 Find The First Player to win K Games in a Row/3175find-the-first-player-to-win-k-games-in-a-row.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findWinningPlayer(self, skills: List[int], k: int) -> int:
+        
+        champSkill, champion, tally = skills.pop(0), 0, 0
+
+        for i, skill in enumerate(skills, start = 1):
+            
+            if skill > champSkill: 
+                champion, champSkill, tally = i, skill, 0
+                
+            tally+= 1
+
+            if tally == k: return champion
+        
+        return champion
