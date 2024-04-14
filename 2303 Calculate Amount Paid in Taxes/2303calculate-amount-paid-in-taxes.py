1a06c4a252ae908f5afef2d66d2a21944d643dee
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-04-14 15:15:36 -0400

    Code challenge submission for question: 2303 Calculate Amount Paid in Taxes

---
+++ 2303 Calculate Amount Paid in Taxes/2303calculate-amount-paid-in-taxes.py
@@ -0,0 +1,18 @@
+"""class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        ans = prev = 0
+        for hi, pct in brackets:
+            hi = min(hi,income)
+            ans+= (hi-prev)*pct/100
+            prev = hi
+        return ans"""
+
+class Solution:
+    def calculateTax(self, brackets, income):
+        ans = 0
+        prev = 0
+        for hi, pct in brackets:
+            hi= min(hi, income)
+            ans+=(hi-prev)*pct/100
+            prev = hi
+        return ans
