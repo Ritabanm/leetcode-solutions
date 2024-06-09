3780e0184f42e499f64b40162d971c633f580a1c
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-06-09 10:26:02 -0400

    Code challenge submission for question: 917 Reverse Only Letters

---
+++ 917 Reverse Only Letters/917reverse-only-letters.py
@@ -0,0 +1,22 @@
+"""class Solution:
+    def reverseOnlyLetters(self, s):
+        letters = [c for c in s if c.isalpha()]
+        ans = []
+
+        for c in s:
+            if c.isalpha():
+                ans.append(letters.pop())
+            else:
+                ans.append(c)
+        return "".join(ans)"""
+
+class Solution:
+    def reverseOnlyLetters(self, s):
+        letters = [c for c in s if c.isalpha()]
+        ans = []
+        for c in s:
+            if c.isalpha():
+                ans.append(letters.pop())
+            else:
+                ans.append(c)
+        return "".join(ans)
