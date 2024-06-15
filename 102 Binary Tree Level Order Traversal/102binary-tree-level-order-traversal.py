d2cd4d833d635bd4da8d64ef4f93ceb5d88b22ea
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-06-14 20:20:10 -0400

    Code challenge submission for question: 102 Binary Tree Level Order Traversal

---
+++ 102 Binary Tree Level Order Traversal/102binary-tree-level-order-traversal.py
@@ -0,0 +1,18 @@
+class Solution:
+    def levelOrder(self, root):
+        levels = []
+        if not root:
+            return levels
+        
+        def helper(node, level):
+            if len(levels)==level:
+                levels.append([])
+            levels[level].append(node.val)
+            if node.left:
+                helper(node.left, level+1)
+            if node.right:
+                helper(node.right, level+1)
+        helper(root,0)
+        return levels
+
+        #T:O(N), S:O(N)
