57ce47612b68921b9648b75ddadf2ca8126aa4ff
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-06-19 15:53:21 -0400

    Code challenge submission for question: 3997 Count Dominant Nodes in a Binary Tree

---
+++ 3997 Count Dominant Nodes in a Binary Tree/3997count-dominant-nodes-in-a-binary-tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countDominantNodes(self, root: TreeNode | None) -> int:
+        count = 0
+
+        def dfs(root):
+            nonlocal count
+
+            if not root:
+                return float('-inf')
+
+            # Maximum values from left and right subtree
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+
+            maxChild = max(leftMax, rightMax)
+
+            # Current node is dominant
+            if root.val >= maxChild:
+                count += 1
+
+            # Return maximum value in current subtree
+            return max(root.val, maxChild)
+
+        dfs(root)
+        return count
