deeccceb67128ee1c09f96aa8c199072401f11df
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-06-28 16:17:23 -0400

    Code challenge submission for question: 2648 Generate Fibonacci Sequence

---
+++ 2648 Generate Fibonacci Sequence/2648generate-fibonacci-sequence.js
@@ -0,0 +1,15 @@
+var fibGenerator = function() {
+    // Determine the number of Fibonacci numbers to generate
+    const lengthOfSequence = 50;
+
+    const fibonacciSequence = Array(lengthOfSequence).fill(0);
+
+    fibonacciSequence[1] = 1;
+
+    for (let i = 2; i < fibonacciSequence.length; i++) {
+        fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+    }
+
+    // Return an iterator for the array
+    return fibonacciSequence[Symbol.iterator]();
+};
