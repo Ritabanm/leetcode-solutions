c3542b4ce92f67da0aebae94fb6867fc6cbdf131
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-07-02 14:19:37 -0400

    Code challenge submission for question: 4014 Minimum Total Price After Applying Discounts

---
+++ 4014 Minimum Total Price After Applying Discounts/4014minimum-total-price-after-applying-discounts.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minPrice(self, prices: list[int], discounts: list[int]) -> float:
+        prices.sort()
+        discounts.sort(reverse = True)
+        total = 0
+        n = min(len(prices), len(discounts))
+        for i in range(n):
+            total+=prices[~i] * (100- discounts[i])/100.0
+        for i in range(n, len(prices)):
+            total+=prices[~i]
+        return float(total)
