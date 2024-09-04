8e220f95375d3cd00d08163fcde096352031cef1
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-09-04 14:14:24 -0400

    Code challenge submission for question: 2783 Flight Occupancy and Waitlist Analysis

---
+++ 2783 Flight Occupancy and Waitlist Analysis/2783flight-occupancy-and-waitlist-analysis.sql
@@ -0,0 +1,17 @@
+SELECT 
+  f.flight_id, 
+  LEAST(
+    f.capacity, 
+    COUNT(p.passenger_id)
+  ) AS booked_cnt, 
+  GREATEST(
+    0, 
+    COUNT(p.passenger_id) - f.capacity
+  ) AS waitlist_cnt 
+FROM 
+  Flights f 
+  LEFT JOIN Passengers p ON f.flight_id = p.flight_id 
+GROUP BY 
+  f.flight_id 
+ORDER BY 
+  f.flight_id;
