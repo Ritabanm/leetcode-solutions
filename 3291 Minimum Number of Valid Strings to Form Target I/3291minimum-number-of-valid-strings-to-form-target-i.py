922a0823b008696c8115d3517418f01c8a70f955
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-09-06 10:16:37 -0400

    Code challenge submission for question: 3291 Minimum Number of Valid Strings to Form Target I

---
+++ 3291 Minimum Number of Valid Strings to Form Target I/3291minimum-number-of-valid-strings-to-form-target-i.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minValidStrings(self, words: List[str], target: str) -> int:
+        trie = {}
+
+        for word in words:
+            node = trie
+            for ch in word:
+                node = node.setdefault(ch, {})
+
+            node["#"] = word # to mark the end of the word
+
+        n = len(target)
+        dp = [float('inf')] * (n+1)
+        dp[n] = 0
+
+        for i in range(n-1, -1, -1):
+            node = trie
+            for j in range(i, n):
+                if target[j] in node:
+                    node = node[target[j]]
+                else:
+                    break
+
+                dp[i] = min(dp[i], 1 + dp[j+1])
+        
+        return dp[0] if dp[0] < float('inf') else -1
+
+        
+            
