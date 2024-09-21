468395250f94b5f964d4aea1a081db14546cbfae
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-09-20 22:15:10 -0400

    Code challenge submission for question: 781 Rabbits in Forest

---
+++ 781 Rabbits in Forest/781rabbits-in-forest.py
@@ -0,0 +1,14 @@
+from collections import Counter
+import math
+
+class Solution:
+    def numRabbits(self, answers):
+        count = Counter(answers)
+        res = 0
+
+        for x, freq in count.items():
+            group_size = x + 1
+            groups = math.ceil(freq / group_size)
+            res += groups * group_size
+
+        return res
