d380370f096d38b2fc07fcbdce786f16df2bf15c
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-10-06 18:41:41 -0400

    Code challenge submission for question: 3931 Check Adjacent Digit Differences

---
+++ 3931 Check Adjacent Digit Differences/3931check-adjacent-digit-differences.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAdjacentDiffAtMostTwo(self, s: str) -> bool:
+        for i in range(1, len(s)):
+            diff = abs(int(s[i]) - int(s[i - 1]))
+            if diff > 2:
+                return False
+        return True
