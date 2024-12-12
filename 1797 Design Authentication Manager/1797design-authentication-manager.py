5dd3eea92084e1ee69490a1a891ea0a6c93b99d8
Ritaban Mitra <ritabanmitra99@gmail.com>
2024-12-12 19:21:23 -0400

    Code challenge submission for question: 1797 Design Authentication Manager

---
+++ 1797 Design Authentication Manager/1797design-authentication-manager.py
@@ -0,0 +1,22 @@
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.ttl  = timeToLive
+        self.expiry = {}
+
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        self.expiry[tokenId] = currentTime + self.ttl
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        
+        if tokenId in self.expiry and self.expiry[tokenId] > currentTime:
+            self.expiry[tokenId] = currentTime + self.ttl
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        count = 0
+        for token in self.expiry:
+            if self.expiry[token] > currentTime:
+                count += 1
+        
+        return count
