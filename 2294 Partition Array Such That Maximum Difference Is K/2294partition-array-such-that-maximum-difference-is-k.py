5a36050a2f19a8149422827c348f7b6c95ba3de2
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-01-15 17:04:28 -0400

    Code challenge submission for question: 2294 Partition Array Such That Maximum Difference Is K

---
+++ 2294 Partition Array Such That Maximum Difference Is K/2294partition-array-such-that-maximum-difference-is-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 1
+
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            diff = nums[i]-start
+            if diff>k:
+                # if diff of starting & curr ele of subseq <K, start new only then. 
+                ans+=1
+                start = nums[i]
+        return ans
