413422fb24b73d8b0f5403ba77e334079768629e
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-04-10 22:42:05 -0400

    Code challenge submission for question: 3054 Binary Tree Nodes

---
+++ 3054 Binary Tree Nodes/3054binary-tree-nodes.sql
@@ -0,0 +1,19 @@
+SELECT
+  N,
+  CASE
+    WHEN P IS NULL THEN
+      "Root"
+    WHEN N IN (
+      SELECT 
+        P 
+      FROM 
+        Tree
+    ) THEN
+      "Inner"
+    ELSE
+      "Leaf"
+    END as Type
+FROM 
+  Tree
+ORDER BY 
+  N;
