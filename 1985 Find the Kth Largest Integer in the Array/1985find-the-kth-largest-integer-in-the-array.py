c9f66db76e76746ade49e0692fc5882fa2be5c2a
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-04-25 19:47:02 -0400

    Code challenge submission for question: 1985 Find the Kth Largest Integer in the Array

---
+++ 1985 Find the Kth Largest Integer in the Array/1985find-the-kth-largest-integer-in-the-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        
+        res = []
+        
+        for i in nums:
+            res.append(int(i))
+        
+        res.sort(reverse = True)
+        
+        return str(res[k-1])
