2929979b534ce89b0d0ca8f3a42b1af988557975
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-07-08 10:33:51 -0400

    Code challenge submission for question: 57 Insert Interval

---
+++ 57 Insert Interval/57insert-interval.py
@@ -0,0 +1,22 @@
+class Solution:
+    def insert(self,intervals, newInterval):
+        n = len(intervals)
+        i = 0
+        res = []
+        #Case 1: no overlap before merging.
+        while i<n and intervals[i][1]<newInterval[0]:
+            res.append(intervals[i])
+            i+=1
+
+        #Case 2: Overlap and merging interval
+        while i<n and newInterval[1]>=intervals[i][0]:
+            newInterval[0]=min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i+=1
+        res.append(newInterval)
+
+        #Case 3: No overlapping after merging newInterval
+        while i<n:
+            res.append(intervals[i])
+            i+=1
+        return res
