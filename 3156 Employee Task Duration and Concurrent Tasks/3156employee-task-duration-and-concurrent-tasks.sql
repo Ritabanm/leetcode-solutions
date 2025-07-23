3e2688470589557ed9dd27aeb26baf363d9219fa
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-07-23 10:10:16 -0400

    Code challenge submission for question: 3156 Employee Task Duration and Concurrent Tasks

---
+++ 3156 Employee Task Duration and Concurrent Tasks/3156employee-task-duration-and-concurrent-tasks.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+WITH events AS (
+    SELECT employee_id,
+           start_time AS event_time,
+           1 as event_status
+    FROM Tasks
+    UNION ALL
+    SELECT employee_id,
+           end_time AS event_time,
+           -1 as event_status
+    FROM Tasks
+    ORDER BY employee_id, event_time, event_status
+)
+
+SELECT employee_id,
+       FLOOR(SUM(TIMESTAMPDIFF(SECOND, start_time, end_time)) / 3600) AS total_task_hours,
+       MAX(event_count) AS max_concurrent_tasks
+FROM (
+    SELECT
+        employee_id,
+        CASE WHEN event_status > 0 AND @event_count = 0 THEN @start_time := event_time
+             ELSE @start_time END AS start_time,
+        @event_count := @event_count + event_status AS event_count,
+        CASE WHEN event_status < 0 AND @event_count = 0 THEN event_time
+             ELSE @start_time END AS end_time
+    FROM events
+    JOIN (SELECT @event_count := 0, @start_time := 0) e
+) e1
+GROUP BY employee_id
+ORDER BY employee_id
