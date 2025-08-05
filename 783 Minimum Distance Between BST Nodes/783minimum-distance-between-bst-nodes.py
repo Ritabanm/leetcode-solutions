a75626e1d18cfade66a495286bf288b44abc5de6
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-08-04 20:39:23 -0400

    Code challenge submission for question: 783 Minimum Distance Between BST Nodes

---
+++ 783 Minimum Distance Between BST Nodes/783minimum-distance-between-bst-nodes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        r = []
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                r.append(node.val)
+                inorder(node.right)
+        inorder(root)
+        mini = float('inf')
+        for i in range(len(r) - 1):
+            mini = min(mini, r[i + 1] - r[i])
+        return mini
