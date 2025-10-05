69393342d5bd65e21518bcb3cbd970a88ec14370
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-10-05 19:16:59 -0400

    Code challenge submission for question: 3273 Minimum Amount of Damage Dealt to Bob

---
+++ 3273 Minimum Amount of Damage Dealt to Bob/3273minimum-amount-of-damage-dealt-to-bob.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDamage(self, power: int, damage: List[int], health: List[int]) -> int:
+        n = len(damage)
+        for i in range(n):
+            health[i]=(health[i]+power-1)//power
+        indexes = sorted(
+            range(n), key = cmp_to_key(lambda i,j:health[i]*damage[j]-
+            health[j]*damage[i]), 
+        )
+        res = 0
+        curTime = 0
+        for i in indexes:
+            curTime+=health[i]
+            res += curTime*damage[i]
+        return res
+        
