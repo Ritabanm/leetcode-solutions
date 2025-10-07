97342413b7a93449085d6bbfd7a78fa31f02a984
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-10-06 22:48:35 -0400

    Code challenge submission for question: 1312 Minimum Insertion Steps to Make a String Palindrome

---
+++ 1312 Minimum Insertion Steps to Make a String Palindrome/1312minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lcs(self, s1: str, s2: str, m: int, n: int) -> int:
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i == 0 or j == 0:
+                    # One of the two strings is empty.
+                    dp[i][j] = 0
+                elif s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[m][n]
+
+    def minInsertions(self, s: str) -> int:
+        n = len(s)
+        sReverse = s[::-1]
+
+        return n - self.lcs(s, sReverse, n, n)
