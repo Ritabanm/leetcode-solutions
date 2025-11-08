26a8a410ad58931f2b8daf6015fafb7c388eeb22
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-11-08 15:28:03 -0400

    Code challenge submission for question: 3411 Maximum Subarray With Equal Products

---
+++ 3411 Maximum Subarray With Equal Products/3411maximum-subarray-with-equal-products.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxLength(self, nums: List[int]) -> int:
+        maxi =1
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                arr = nums[i:j+1]
+                gcd = reduce(math.gcd, arr)
+                lcm = reduce(math.lcm, arr)
+                product = math.prod(arr)
+                if product == gcd*lcm:
+                    maxi = max(len(arr), maxi)
+        return maxi
