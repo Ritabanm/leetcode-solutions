1993ba5e667cba76570bf47184773cea6db9eb3f
Ritaban Mitra <ritabanmitra99@gmail.com>
2025-12-15 16:44:23 -0400

    Code challenge submission for question: 2498 Frog Jump II

---
+++ 2498 Frog Jump II/2498frog-jump-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxJump(self, stones: List[int]) -> int:
+        if len(stones)==2:
+            return stones[1]-stones[0]
+        
+        jump = stones[0]
+        for i in range(len(stones)-2):
+            jump = max(jump, stones[i+2]-stones[i])
+        return jump
