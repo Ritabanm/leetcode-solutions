972a46f56715e0dd84cd3dfe0358e5c9284aca78
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-01-15 17:36:51 -0400

    Code challenge submission for question: 2796 Repeat String

---
+++ 2796 Repeat String/2796repeat-string.ts
@@ -0,0 +1,13 @@
+declare global {
+    interface String {
+        replicate(times: number): string;
+    }
+}
+
+String.prototype.replicate = function(times: number) {
+    if (times === 0) {
+        return "";
+    }
+    //Here "this" refers to the string object on which the method is called
+    return this + this.replicate(times - 1);
+};
