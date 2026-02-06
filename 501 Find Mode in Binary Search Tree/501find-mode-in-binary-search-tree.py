d94f6f870fefd5302a5445db3c89181cfe57b722
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-02-06 19:35:38 -0400

    Code challenge submission for question: 501 Find Mode in Binary Search Tree

---
+++ 501 Find Mode in Binary Search Tree/501find-mode-in-binary-search-tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMode(self, root:Optional[TreeNode]):
+        def dfs(node, counter):
+            if not node:
+                return 
+            
+            counter[node.val]+=1
+            dfs(node.left, counter)
+            dfs(node.right, counter)
+        
+        counter = defaultdict(int)
+        dfs(root, counter)
+        max_freq = max(counter.values())
+
+        ans = []
+        for key in counter:
+            if counter[key]==max_freq:
+                ans.append(key)
+        return ans
