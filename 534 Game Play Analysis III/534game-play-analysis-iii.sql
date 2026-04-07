6c1c1407593c78862a08fd8c85dab91e7334d40e
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-04-07 16:17:55 -0400

    Code challenge submission for question: 534 Game Play Analysis III

---
+++ 534 Game Play Analysis III/534game-play-analysis-iii.sql
@@ -0,0 +1,11 @@
+SELECT
+  A.player_id,
+  A.event_date,
+  SUM(A.games_played) OVER (
+    PARTITION BY
+      A.player_id
+    ORDER BY
+      A.event_date
+  ) AS games_played_so_far
+FROM
+  Activity A;
