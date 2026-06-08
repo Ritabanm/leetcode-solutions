f2e911967422b82d95133f467f14976ac5a48957
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-06-07 22:56:12 -0400

    Code challenge submission for question: 1299 Replace Elements with Greatest Element on Right Side

---
+++ 1299 Replace Elements with Greatest Element on Right Side/1299replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,10 @@
+class Solution:
+    def replaceElements(self, arr):
+        Max = arr[-1]
+        arr[-1]=-1
+
+        for i in range(len(arr)-2,-1,-1):
+            temp = arr[i]
+            arr[i] = Max
+            if temp>Max: Max=temp
+        return arr
