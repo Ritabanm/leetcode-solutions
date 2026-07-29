95aee5800493dad0e0c506d3e2356661d4ccda64
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-07-28 21:15:37 -0400

    Code challenge submission for question: 2967 Minimum Cost to Make Array Equalindromic

---
+++ 2967 Minimum Cost to Make Array Equalindromic/2967minimum-cost-to-make-array-equalindromic.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        
+        nums.sort()
+        pal = lambda x: str(x) == str(x)[::-1]
+        left = rght = nums[len(nums)//2]
+
+        while not pal(left): left-= 1
+        while not pal(rght): rght+= 1
+
+        return min(sum(map(lambda x: abs(x - left), nums)),
+                   sum(map(lambda x: abs(x - rght), nums)))
