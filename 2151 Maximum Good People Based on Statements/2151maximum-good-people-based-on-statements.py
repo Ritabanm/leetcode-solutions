165f979a9380dec1d29d741fe23203c9c5b69b97
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-08-15 15:08:10 -0400

    Code challenge submission for question: 2151 Maximum Good People Based on Statements

---
+++ 2151 Maximum Good People Based on Statements/2151maximum-good-people-based-on-statements.py
@@ -0,0 +1,25 @@
+class Solution:        
+    def maximumGood(self, statements: List[List[int]]) -> int:
+        
+        n = len(statements)
+        res = 0
+        
+        def checkGood(person, mask):
+            if (mask>>person) & 1:
+                return True
+            return False
+        
+        def validateStatement(mask):
+            for i in range(n):
+                if checkGood(i, mask):
+                    for j in range(n):
+                        statement = statements[i][j]
+                        
+                        if (statement==0 and checkGood(j, mask)) or (statement==1 and not checkGood(j, mask)):
+                            return False
+            return True
+        
+        for i in range(1,1<<n): 
+            if validateStatement(i):
+                res = max(res, bin(i).count('1'))
+        return res
