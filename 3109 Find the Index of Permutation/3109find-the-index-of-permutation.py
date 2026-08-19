8bcb9a8a64cac8baf8182d543bfbe97c8646aeee
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-08-19 12:50:09 -0400

    Code challenge submission for question: 3109 Find the Index of Permutation

---
+++ 3109 Find the Index of Permutation/3109find-the-index-of-permutation.py
@@ -0,0 +1,26 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def getPermutationIndex(self, perm: List[int]) -> int:
+
+        MOD = 10 ** 9 + 7
+
+        count = [0] * len(perm) # values still avaiable and also less than cur-value
+        stack = SortedList()
+        for i in range(len(perm)):
+            index = stack.bisect_left(perm[i])
+            count[i] = perm[i] - 1 - index
+            stack.add(perm[i])
+
+        @lru_cache(None)
+        def frac(num):
+            if num <= 1:
+                return 1
+            return num * frac(num - 1) % MOD
+
+        ans = 0
+        for i in range(len(perm)):
+            ans += count[i] * frac(len(perm) - i - 1)
+            ans = ans % MOD
+
+        return ans
