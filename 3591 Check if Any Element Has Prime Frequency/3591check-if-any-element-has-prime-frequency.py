602098a1a6f884330e560e1f1942f7a7af264d34
Ritaban Mitra <ritabanmitra99@gmail.com>
2026-09-23 13:29:39 -0400

    Code challenge submission for question: 3591 Check if Any Element Has Prime Frequency

---
+++ 3591 Check if Any Element Has Prime Frequency/3591check-if-any-element-has-prime-frequency.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkPrimeFrequency(self, nums: List[int]) -> bool:
+        cntr=Counter(nums) 
+        for val in cntr.values():
+            if val in (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 , 97):
+                return True
+        return False
